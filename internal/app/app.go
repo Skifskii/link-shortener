9bf126597564b1549e632217bf5c07411a1670b9
Skifskii <petya-koreshkov@yandex.ru>
2025-08-27 19:48:12 +0300

    Вынес инициализацию зависимостей в пакет app

---
+++ internal/app/app.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Skifskii/link-shortener/internal/config"
+	"github.com/Skifskii/link-shortener/internal/handler/redirect"
+	"github.com/Skifskii/link-shortener/internal/handler/save"
+	"github.com/Skifskii/link-shortener/internal/repository"
+	"github.com/Skifskii/link-shortener/internal/repository/inmemory"
+	"github.com/Skifskii/link-shortener/internal/service/shortener"
+	"github.com/go-chi/chi/v5"
+)
+
+func Run() error {
+	cfg := config.New()
+	var repo repository.Repository = inmemory.New()
+	s := shortener.New(6)
+
+	r := chi.NewRouter()
+	r.Get("/{id}", redirect.New(repo))
+	r.Post("/", save.New(repo, s, cfg.BaseURL))
+
+	fmt.Printf("Starting server at %s\n", cfg.Address)
+	return http.ListenAndServe(cfg.Address, r)
+}
