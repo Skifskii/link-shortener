556a295f41b7871b0ff4802c2429f96ff2cf7d9e
Skifskii <petya-koreshkov@yandex.ru>
2025-10-10 22:57:23 +0300

    Убрал foreign keys чтобы пройти тесты

--- migrations/000004_create_users_links_table.up.sql
+++ migrations/000004_create_users_links_table.up.sql
@@ -1,6 +1,6 @@
 -- Создание промежуточной таблицы пользователей и ссылок
 CREATE TABLE users_links (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    link_id INTEGER NOT NULL REFERENCES links(id) ON DELETE CASCADE
+    user_id INTEGER NOT NULL,
+    link_id INTEGER NOT NULL
 );
